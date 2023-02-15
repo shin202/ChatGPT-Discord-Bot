6fd2c7f97d8349b22ea66978c8be38ef4273ee91
Tran Viet Hoang <tranviethoang642@gmail.com>
2023-02-16 01:01:56 +0700

    feat!: add ask and changePrefix command

---
+++ src/commands/ask.ts
@@ -0,0 +1,40 @@
+import {ICommand} from "../types/types";
+import UserController from "../controllers/UserController";
+import ConversationController from "../controllers/ConversationController";
+import ChatGPTService from "../services/ChatGPTService";
+import MessageController from "../controllers/MessageController";
+
+const command: ICommand = {
+    name: "ask",
+    execute: async (message, args) => {
+        const user = await UserController.getUserByDiscordId(message.author.id);
+        const inConversation = await ConversationController.inConversation(user);
+
+        if (!inConversation) {
+           await message.reply('You are not in conversation or the conversation session is expired.\nPlease start a new conversation to use this command.');
+           return;
+        }
+
+        const currentChatMode = await ConversationController.getCurrentChatMode(user);
+        if (!args[1]) {
+            const { welcomeMessage } = ChatGPTService.getChatModeInfo(currentChatMode);
+            await message.reply(welcomeMessage);
+            return;
+        }
+
+        const userMessage = args.slice(1).join(" ");
+        const resMessage = await ChatGPTService.generateCompletion(userMessage, user, currentChatMode);
+        await MessageController.createNewMessage(user, {
+           chatMode: currentChatMode!,
+           userMessage: userMessage,
+           botMessage: resMessage,
+        });
+        await message.channel.sendTyping();
+        await message.reply(resMessage);
+    },
+    permissions: [],
+    aliases: ["chat"],
+    cooldown: 5,
+}
+
+export default command;
