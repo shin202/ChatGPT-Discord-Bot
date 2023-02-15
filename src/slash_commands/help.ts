e905290b8aef0b7c80510db85456f246897c0ab0
Tran Viet Hoang <tranviethoang642@gmail.com>
2023-02-16 01:00:05 +0700

    chore: change static prefix

--- src/slash_commands/help.ts
+++ src/slash_commands/help.ts
@@ -1,12 +1,17 @@
-import { SlashCommandBuilder } from "discord.js";
-import { ISlashCommand } from "../types/types";
+import {SlashCommandBuilder} from "discord.js";
+import {ISlashCommand} from "../types/types";
 import {HelpMessageEmbed} from "../embed/HelpMessageEmbed";
+import GuildController from "../controllers/GuildController";
 
 const command: ISlashCommand = {
     command: new SlashCommandBuilder()
         .setName("help")
         .setDescription("Show help."),
     execute: async (interaction) => {
+        let prefix = "";
+        const guildPrefix = await GuildController.getGuildOption(interaction.guild?.id!, "prefix") as string;
+        prefix = guildPrefix ?? process.env.PREFIX;
+
         const HELP_MESSAGES = `📍 Slash Commands: 
             🛠 /start - Start a new conversation
             🛠 /mode - Select chat mode
@@ -15,7 +20,7 @@ const command: ISlashCommand = {
             🛠 /clear - Clear all user message
             \n
             📎 Prefix Commands:
-            !ask - Start asking ChatGPT bot
+            ${prefix}ask - Start asking ChatGPT bot
         `;
 
         await interaction.reply({
